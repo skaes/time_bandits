9814a59a8d6e956941405f6f141ffaa6f0a6f8c2
David Anderson <david@folklogic.com>
2010-02-02 13:49:53 +0100

    during initialization, enable memcache and GC stats if they are available

--- init.rb
+++ init.rb
@@ -3,3 +3,6 @@ require 'time_bandits'
 ActionController::Base.send :include, ActionController::TimeBanditry
 
 TimeBandits::TimeConsumers::GarbageCollection.heap_dumps_enabled = %w(production development).include?(RAILS_ENV)
+
+TimeBandits.add TimeBandits::TimeConsumers::Memcached if defined?(Memcached)
+TimeBandits.add TimeBandits::TimeConsumers::GarbageCollection.instance if GC.respond_to? :enable_stats
