c9a10c324c0202b10159bdaa4d07cad06ae2c9ec
Stefan Kaes <skaes@railsexpress.de>
2011-03-29 17:27:49 +0200

    don't install time bandits per default (app needs control over order of bandits in completed line)

--- init.rb
+++ init.rb
@@ -14,6 +14,6 @@ end
 
 TimeBandits::TimeConsumers::GarbageCollection.heap_dumps_enabled = %w(production development).include?(Rails.env)
 
-TimeBandits.add TimeBandits::TimeConsumers::Memcached if defined?(Memcached)
-TimeBandits.add TimeBandits::TimeConsumers::GarbageCollection.instance if GC.respond_to? :enable_stats
-TimeBandits.add TimeBandits::TimeConsumers::JMX.instance if defined? JRUBY_VERSION
+# TimeBandits.add TimeBandits::TimeConsumers::Memcached if defined?(Memcached)
+# TimeBandits.add TimeBandits::TimeConsumers::GarbageCollection.instance if GC.respond_to? :enable_stats
+# TimeBandits.add TimeBandits::TimeConsumers::JMX.instance if defined? JRUBY_VERSION
