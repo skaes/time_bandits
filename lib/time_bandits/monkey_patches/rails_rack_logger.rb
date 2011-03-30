47c49de4caf20f30317c642ddcf46d76646b7532
Stefan Kaes <skaes@railsexpress.de>
2011-03-30 16:09:02 +0200

    refactored rack logger

--- lib/time_bandits/monkey_patches/rails_rack_logger.rb
+++ lib/time_bandits/monkey_patches/rails_rack_logger.rb
@@ -5,23 +5,17 @@ module Rails
     class Logger
 
       def call(env)
-        Thread.current[:time_bandits_completed_info] = nil
         start_time = before_dispatch(env)
         result = @app.call(env)
       ensure
         run_time = Time.now - start_time
-        status = result.first
-        duration, additions = Thread.current[:time_bandits_completed_info]
-        (additions ||= []).insert(0, "Controller: %.1fms" % duration)
-
-        message = "Completed #{status} #{::Rack::Utils::HTTP_STATUS_CODES[status]} in %.1fms (#{additions.join(' | ')})" % (run_time*1000)
-        info message
-
-        after_dispatch(env)
+        after_dispatch(env, result, run_time)
       end
 
       protected
       def before_dispatch(env)
+        Thread.current[:time_bandits_completed_info] = nil
+
         start_time = Time.now
         request = ActionDispatch::Request.new(env)
         path = request.fullpath
@@ -29,6 +23,16 @@ module Rails
         info "\n\nStarted #{request.request_method} \"#{path}\" " \
              "for #{request.ip} at #{start_time.to_default_s}"
         start_time
+      end
+
+      def after_dispatch(env, result, run_time)
+        status = result.first
+        duration, additions = Thread.current[:time_bandits_completed_info]
+
+        message = "Completed #{status} #{::Rack::Utils::HTTP_STATUS_CODES[status]} in %.1fms (#{additions.join(' | ')})" % (run_time*1000)
+        info message
+
+        ActiveSupport::LogSubscriber.flush_all!
       end
 
     end
