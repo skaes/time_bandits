cf9c766dfa08647bf5b12dbb4dd6327f5a8a4c8d
Pascal Friederich <pascal.friederich@xing.com>
2011-04-05 16:33:49 +0200

    Fail gracefully if an exception occurs during before_dispatch

--- lib/time_bandits/monkey_patches/rails_rack_logger.rb
+++ lib/time_bandits/monkey_patches/rails_rack_logger.rb
@@ -5,7 +5,8 @@ module Rails
     class Logger
 
       def call(env)
-        start_time = before_dispatch(env)
+        start_time = Time.now
+        before_dispatch(env, start_time)
         result = @app.call(env)
       ensure
         run_time = Time.now - start_time
@@ -13,20 +14,18 @@ module Rails
       end
 
       protected
-      def before_dispatch(env)
+      def before_dispatch(env, start_time)
         Thread.current[:time_bandits_completed_info] = nil
 
-        start_time = Time.now
         request = ActionDispatch::Request.new(env)
         path = request.fullpath
 
         info "\n\nStarted #{request.request_method} \"#{path}\" " \
              "for #{request.ip} at #{start_time.to_default_s}"
-        start_time
       end
 
       def after_dispatch(env, result, run_time)
-        status = result.first
+        status = result.first || 500
         duration, additions = Thread.current[:time_bandits_completed_info]
 
         message = "Completed #{status} #{::Rack::Utils::HTTP_STATUS_CODES[status]} in %.1fms" % (run_time*1000)
