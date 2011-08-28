107117f27139218978c08810cd3d7ded4040d9a4
Stefan Kaes <stefan.kaes@xing.com>
2011-08-28 11:35:26 +0200

    removed last traces of agent suport

--- lib/time_bandits.rb
+++ lib/time_bandits.rb
@@ -55,7 +55,6 @@ module TimeBandits
     consumed # needs to be called for DB time consumer
     rc = e ? "500 Internal Server Error" : "200 OK"
     logger.info "#{title} #{sprintf("%.3f", seconds * 1000)}ms (#{runtime}) | #{rc}"
-    logger.agent[:benchmarks] = metrics.merge!(:response_code => rc) if logger.respond_to?(:agent)
     raise e if e
     result
   end
