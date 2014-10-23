f3f30806eab59756dbfb6fa7dd5cb3490d048922
Stefan Kaes <stefan.kaes@xing.com>
2014-10-23 14:54:12 +0200

    style change

--- lib/time_bandits/monkey_patches/sequel.rb
+++ lib/time_bandits/monkey_patches/sequel.rb
@@ -1,8 +1,9 @@
 require 'sequel'
 
-major, minor, patch = Sequel.version.split('.').map(&:to_i)
-raise "time_bandits Sequel monkey patch is not compatible with your sequel version" if
-  major < 4 || (major == 4 && minor < 15)
+major, minor, _ = Sequel.version.split('.').map(&:to_i)
+if major < 4 || (major == 4 && minor < 15)
+  raise "time_bandits Sequel monkey patch is not compatible with your sequel version"
+end
 
 Sequel::Database.class_eval do
   alias :_orig_log_yield :log_yield
